ed0589af4dca8a4c07b8255c6cfe8912335b0d58
Jordan England-Nelson <JordanEnglandNelson@Jordans-MacBook-Air.local>
2017-04-06 05:25:06 -0700

    Fixes image sizes and spacing

--- imports/ui/screens/HomePage.jsx
+++ imports/ui/screens/HomePage.jsx
@@ -115,17 +115,8 @@ class HomePage extends React.Component {
 
                 <div className="col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 text-center">
 
-                  <h1 className="section-title">About</h1>
-
-                  <div className="text-left scrollimation fade-left">
-                    <p>I am a software engineer with experience developing full-stack web applications using <span className="main-color">JavaScript</span>, <span className="main-color">jQuery</span>, <span className="main-color">Node</span>, RESTful APIs, web sockets and <span className="main-color">Bootstrap</span>. I also have a background in journalism, digital marketing and product development.</p>
-                    <p>Journalism taught me to empathize with users and to distill technical information for a lay audience. These skills have mapped well to the roles I’ve held in tech: user researcher, content marketer, technical writer, software developer.</p>
-                    <p>Five years living in Europe taught me how to work with people from different backgrounds and cultures, thrive in unfamiliar environments, and pick up new languages.</p>
-                    <p>Full working proficiency in Spanish, French, and JavaScript.</p>
-                  </div>
-
                   {/*Social media*/}
-                  <div>
+                  <div className="social-icon-list">
                     <div className="social-media-icon">
                       <a href="https://www.linkedin.com/in/jordan-england-nelson-05283494" data-toggle="tooltip" title="Follow me on LinkedIn" target="_blank"><h2><i className="fa fa-fw fa-linkedin"></i></h2></a>
                     </div>
@@ -137,6 +128,13 @@ class HomePage extends React.Component {
                     <div className="social-media-icon">
                       <a href="https://twitter.com/jordanenelson" data-toggle="tooltip" title="Follow me on Twitter" target="_blank"><h2><i className="fa fa-fw fa-twitter"></i>      </h2></a>
                     </div>
+                  </div>
+
+                  <div className="text-left scrollimation fade-left">
+                    <p>I am a software engineer with experience developing full-stack web applications using <span className="main-color">JavaScript</span>, <span className="main-color">jQuery</span>, <span className="main-color">React</span>, <span className="main-color">Node</span>, RESTful APIs, web sockets and <span className="main-color">Bootstrap</span>. I also have a background in journalism, digital marketing and product development.</p>
+                    <p>Journalism taught me to empathize with users and to distill technical information for a lay audience. These skills have mapped well to the roles I’ve held in tech: user researcher, content marketer, technical writer, software developer.</p>
+                    <p>Five years living in Europe taught me how to work with people from different backgrounds and cultures, thrive in unfamiliar environments, and pick up new languages.</p>
+                    <p>Full working proficiency in Spanish, French, and JavaScript.</p>
                   </div>
 
 
@@ -158,11 +156,10 @@ class HomePage extends React.Component {
     				<div className="container">
 
     					<h1 className="section-title">Code</h1>
-              <hr />
+
+              {/* Special Ed Goal Tracker */}
     					<div className="row">
-
     						<div className="col-sm-5 scrollimation fade-right">
-
     							<h2 className="row-title">SpecialEd Goal Tracker</h2>
                   <div className="text-left">
                     <p>Web app that helps teachers manage academic and behavioral goals for students with special needs.</p>
@@ -170,39 +167,31 @@ class HomePage extends React.Component {
                     <p>Currently user testing at the Orange County Department of Education's primary special needs campus in Fountain Valley.</p>
                     <p><a className="btn btn-dc btn-small btn-main-color source-code-button" href="https://github.com/PlayaLink/iep-goal-tracker">View project <i className="fa fa-fw fa-github"></i></a></p>
                   </div>
-
-
-
     						</div>
+                <div className="project-image">
+      						<div className="col-xs-4 col-sm-2 col-sm-offset-1 scrollimation fade-left">
+      							<img className="img-responsive" src="assets/images/iphone/dashboard_facing_right1.png" alt="" />
+      						</div>
 
-    						<div className="col-xs-4 col-sm-2 col-sm-offset-1 scrollimation fade-left">
+      						<div className="col-xs-4 col-sm-2 scrollimation fade-left">
+      							<img className="img-responsive" src="assets/images/iphone/goal_detail_portrait1.png" alt="" />
+      						</div>
 
-    							<img className="img-responsive" src="assets/images/iphone/dashboard_facing_right1.png" alt="" />
-
-    						</div>
-
-    						<div className="col-xs-4 col-sm-2 scrollimation fade-left">
-
-    							<img className="img-responsive" src="assets/images/iphone/goal_detail_portrait1.png" alt="" />
-
-    						</div>
-
-    						<div className="col-xs-4 col-sm-2 scrollimation fade-left">
-
-    							<img className="img-responsive" src="assets/images/iphone/evaluation_facing_left1.png" alt="" />
-
-    						</div>
+      						<div className="col-xs-4 col-sm-2 scrollimation fade-left">
+      							<img className="img-responsive" src="assets/images/iphone/evaluation_facing_left1.png" alt="" />
+      						</div>
+                </div>
 
     					</div>
 
-    					<hr style={{marginTop: "40px"}} />
+    					<hr style={{margin: "40px"}} />
 
     					<div className="row">
-
-    						<div className="col-sm-6 col-sm-push-6 push-down scrollimation fade-left">
+    {/*RapiDoc*/}
+    						<div className="col-xs-6 col-sm-3 scrollimation fade-left">
 
     							<h2 className="row-title">RapiDoc</h2>
-                  <div className="text-left">
+                  <div className="text-left project-description">
                     <p>A healthcare marketplace app that helps patients find providers willing to accept discount rates in exchange for paying cash for service.
                     Currently testing with 100 practices.</p>
                     <p>I converted the UI from <span className="main-color">Blaze</span> templates to <span className="main-color">React</span> components.</p>
@@ -210,21 +199,16 @@ class HomePage extends React.Component {
 
     						</div>
 
-    						<div className="col-sm-6 col-sm-pull-6 scrollimation fade-right">
+    						<div className="col-xs-6 col-sm-3 scrollimation fade-right">
                   <div className="row">
                     <div className="col-xs-6 col-xs-offset-3">
-                      <img className="img-responsive" src="assets/images/rapidoc_screenshot_framed.jpg" alt="" />
+                      <img className="img-responsive project-image" src="assets/images/rapidoc_screenshot.jpg" alt="" />
                     </div>
                   </div>
     						</div>
 
-    					</div>
-
-              <hr />
-
-              <div className="row">
-
-    						<div className="col-sm-5 col-sm-offset-1 push-down scrollimation fade-right">
+    {/*Spliffen*/}
+                <div className="col-xs-6 col-sm-3 scrollimation fade-right">
 
     							<h2 className="row-title">Spliffen</h2>
 
@@ -235,10 +219,10 @@ class HomePage extends React.Component {
 
     						</div>
 
-                <div className="col-sm-6 scrollimation fade-left">
+                <div className="col-xs-6 col-sm-3 scrollimation fade-left">
                   <div className="row">
                     <div className="col-xs-6 col-xs-offset-3">
-                      <img className="img-responsive" src="assets/images/spliffen_portrait.png" alt="" />
+                      <img className="img-responsive project-image" src="assets/images/spliffen_screenshot.png" alt="" />
                     </div>
                   </div>
     						</div>
