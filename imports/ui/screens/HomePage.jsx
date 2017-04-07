c5bffa1edfb5dace13a2bae903564466e7c2a9f5
Jordan England-Nelson <JordanEnglandNelson@Jordans-MacBook-Air.local>
2017-04-06 17:06:49 -0700

    Cleans up file system

--- imports/ui/screens/HomePage.jsx
+++ imports/ui/screens/HomePage.jsx
@@ -170,15 +170,15 @@ class HomePage extends React.Component {
     						</div>
                 <div className="project-image">
       						<div className="col-xs-4 col-sm-2 col-sm-offset-1 scrollimation fade-left">
-      							<img className="img-responsive" src="assets/images/iphone/dashboard_facing_right1.png" alt="" />
+      							<img className="img-responsive" src="assets/images/specialEd/dashboard_facing_right1.png" alt="" />
       						</div>
 
       						<div className="col-xs-4 col-sm-2 scrollimation fade-left">
-      							<img className="img-responsive" src="assets/images/iphone/goal_detail_portrait1.png" alt="" />
+      							<img className="img-responsive" src="assets/images/specialEd/goal_detail_portrait1.png" alt="" />
       						</div>
 
       						<div className="col-xs-4 col-sm-2 scrollimation fade-left">
-      							<img className="img-responsive" src="assets/images/iphone/evaluation_facing_left1.png" alt="" />
+      							<img className="img-responsive" src="assets/images/specialEd/evaluation_facing_left1.png" alt="" />
       						</div>
                 </div>
 
